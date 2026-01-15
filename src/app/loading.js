983809da8c110e6d03e6387e31f5dc5e795ef16b
manish202 <godofmind51616@gmail.com>
2026-01-15 17:25:22 +0530

    loading animation added

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return (
+        <div className="d-flex loading_anim">
+            <div className="loader-wrapper">
+                <div className="spinner"></div>
+                <p className="loading-text">Loading...</p>
+            </div>
+        </div>
+    )
+}
+export default Loading;
